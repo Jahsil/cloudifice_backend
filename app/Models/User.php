0e1ce9974d0280bae2e0185d757b833ea5b718d5
Eyouel Haile <eyouel2016@gmail.com>
2025-01-02 16:22:48 +0300

    Revert "add login and register routes together with jwt"
    This reverts commit 94812e9ef40af8327f2a58cfdc0af4664f4928fa.

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,72 +1,48 @@
 <?php
 
 namespace App\Models;
-use Tymon\JWTAuth\Contracts\JWTSubject;
-
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable
 {
-    use Notifiable;
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
 
-     protected $fillable = [
-        'first_name',
-        'last_name',
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
         'email',
-        'phone',
-        'nationality',
         'password',
     ];
 
-    // Add JWT methods
-    public function getJWTIdentifier()
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
     {
-        return $this->getKey();
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
     }
-
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
-    
-    // /** @use HasFactory<\Database\Factories\UserFactory> */
-    // use HasFactory, Notifiable;
-
-    // /**
-    //  * The attributes that are mass assignable.
-    //  *
-    //  * @var list<string>
-    //  */
-    // protected $fillable = [
-    //     'name',
-    //     'email',
-    //     'password',
-    // ];
-
-    // /**
-    //  * The attributes that should be hidden for serialization.
-    //  *
-    //  * @var list<string>
-    //  */
-    // protected $hidden = [
-    //     'password',
-    //     'remember_token',
-    // ];
-
-    // /**
-    //  * Get the attributes that should be cast.
-    //  *
-    //  * @return array<string, string>
-    //  */
-    // protected function casts(): array
-    // {
-    //     return [
-    //         'email_verified_at' => 'datetime',
-    //         'password' => 'hashed',
-    //     ];
-    // }
 }
