46a7bd5d964385906ab485de3d0c7cf454fccce1
Eyouel Haile <eyouel2016@gmail.com>
2025-01-30 11:08:03 +0300

    check cookie info

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -6,6 +6,8 @@ use Closure;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class JwtMiddleware
 {
@@ -18,23 +20,52 @@ class JwtMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
+        // bearer token based middleware 
+        // try {
+        //     $token = $request->bearerToken();
+
+        //     if (!$token) {
+        //         return response()->json(['error' => 'Token not provided'], 401);
+        //     }
+
+        //     $user = JWTAuth::parseToken()->authenticate();
+
+        //     if (!$user) {
+        //         return response()->json(['error' => 'User not found'], 401);
+        //     }
+
+        //     return $next($request);
+
+        // } catch (JWTException $e) {
+        //     return response()->json(['error' => 'Token is invalid or expired'], 401);
+        // }
+
+        // cookie based middlware
+
         try {
-            $token = $request->bearerToken();
+            $token = $request->cookie("token");
 
-            if (!$token) {
-                return response()->json(['error' => 'Token not provided'], 401);
-            }
-
-            $user = JWTAuth::parseToken()->authenticate();
-
-            if (!$user) {
-                return response()->json(['error' => 'User not found'], 401);
-            }
-
-            return $next($request);
-
-        } catch (JWTException $e) {
-            return response()->json(['error' => 'Token is invalid or expired'], 401);
+        if(!$token){
+            return response()->json(['error' => 'Unauthorized - No token found'], Response::HTTP_UNAUTHORIZED);
         }
+
+        // Manually set the token for JWTAuth
+        JWTAuth::setToken($token);
+
+        // Authenticate user
+        $user = JWTAuth::authenticate();
+
+        if (!$user) {
+            return response()->json(['error' => 'Unauthorized - User not found'], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $request->attributes->set('user', $user);
+
+        return $next($request);
+        
+        }catch (JWTException $e) {
+            return response()->json(['error' => 'Unauthorized - Invalid or expired token'], Response::HTTP_UNAUTHORIZED);
+        }
+        
     }
 }
