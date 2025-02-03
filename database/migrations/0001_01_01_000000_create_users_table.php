e3681e9a4c13f1e5eca2a2458af75f7a9b2ee02d
Eyouel Haile <eyouel2016@gmail.com>
2025-02-03 17:04:51 +0300

    add username column to migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-	    $table->string('first_name');
-	    $table->string('last_name');
-	    $table->string('nationality');
-	    $table->string('phone')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('nationality');
+            $table->string('phone')->unique();
             $table->string('email')->unique();
+            $table->string('username')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
