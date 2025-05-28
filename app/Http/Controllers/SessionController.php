3042b1d5e6cfce9d9c4e8b1fb79a8f000079251b
Eyouel Haile <eyouel2016@gmail.com>
2025-05-28 13:19:07 +0300

    add session controller and test token decoding functionality

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Crypt;
 class SessionController extends Controller
 {
     //
-    public function checkToken(Request $request){
+    public function checkToken(){
 
         $encrypted = 'eyJpdiI6ImdaV1pKOWVQbG55WkoxZXdET210TGc9PSIsInZhbHVlIjoiWjN1WXJRdUtIbkgxNzFXSGdXYWdBT0hGOG10clZzQjNhdm1GSmFMYm1iVEdtNlhrQmlRQldNQjY2VlE4V3RYOHJoSUJScm5aQmF2T0lWbXVPU1NhMWxTUHpsTkc1VGFwZjV1dmk0OWJRNVRCZUpoK0V5bmhGeERaajVOTFV2ZzciLCJtYWMiOiJjODU0MTE0ZTFiMTg0ZDAwYTY2MTc1NDliZTAwYTU1MjE3MmYyYmJlZmJlNjJkOGI0OTAwMjQyODFmOWFmNzFmIiwidGFnIjoiIn0%3D';
         
