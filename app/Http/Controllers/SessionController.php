5d0178999cd5acd4f94fcc21549e33d97e2662aa
Eyouel Haile <eyouel2016@gmail.com>
2025-05-28 13:21:17 +0300

    add session controller and test token decoding functionality

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -9,17 +9,22 @@ use Illuminate\Support\Facades\Crypt;
 class SessionController extends Controller
 {
     //
-    public function checkToken(){
-
+    public function checkToken()
+    {
         $encrypted = 'eyJpdiI6ImdaV1pKOWVQbG55WkoxZXdET210TGc9PSIsInZhbHVlIjoiWjN1WXJRdUtIbkgxNzFXSGdXYWdBT0hGOG10clZzQjNhdm1GSmFMYm1iVEdtNlhrQmlRQldNQjY2VlE4V3RYOHJoSUJScm5aQmF2T0lWbXVPU1NhMWxTUHpsTkc1VGFwZjV1dmk0OWJRNVRCZUpoK0V5bmhGeERaajVOTFV2ZzciLCJtYWMiOiJjODU0MTE0ZTFiMTg0ZDAwYTY2MTc1NDliZTAwYTU1MjE3MmYyYmJlZmJlNjJkOGI0OTAwMjQyODFmOWFmNzFmIiwidGFnIjoiIn0%3D';
-        
-        // Decrypt and serialize
-        $decrypted = Crypt::decrypt(urldecode($encrypted));
-        $serialized = serialize($decrypted);
-        $base64 = base64_encode($serialized);
-    
-        response()->json([
-            "base64" => $base64
-        ]);
+
+        try {
+            $decrypted = Crypt::decrypt(urldecode($encrypted));
+
+            return response()->json([
+                "decrypted" => $decrypted
+            ]);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                "error" => "Failed to decrypt",
+                "message" => $e->getMessage()
+            ], 400);
+        }
     }
 }
